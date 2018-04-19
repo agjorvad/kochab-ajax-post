5bab7e55d2f4e5fffe3a341ffe077ff8643a77d8
Alex Gjorvad <alexgjorvad@gmail.com>
2018-04-19 12:01:30 -0500

    added records to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,25 @@
-console.log( 'client.js has been loaded');
+console.log('client.js has been loaded');
 
 $(document).ready(onReady);
 
 function onReady() {
     console.log('jQuery is loaded');
 }
+
+$.ajax({
+    type: 'GET',
+    url: '/records'
+})
+    .then(function (response) {
+        console.log(response);
+        // for (let i = 0; i < response.length; i++) {
+        //     $('ul').append('<li>' + response[i].title + (' ') + response[i].year + (' ') + response[i].artist + (' ') + response[i].cost + '</li>');
+        response.forEach(function (record) {
+            $('#recordList').append(`<tr>
+                <td>${record.title}</td>
+                <td>${record.year}</td>
+                <td>${record.artist}</td>
+                <td>${record.cost}</td>
+                </tr>`);
+        })
+    });
